5c464a9142426282a6496788936803001142a420
LM <cloosingdoors2@gmail.com>
2016-12-27 11:27:23 +0100

    Afegit mapa al detall

--- client/components/detail.js
+++ client/components/detail.js
@@ -1,4 +1,11 @@
 Template.detail.onCreated(function detailOnCreated(){
+  GoogleMaps.ready('locationProperty', function(map) {
+   // Add a marker to the map once it's ready
+   var marker = new google.maps.Marker({
+     position: map.options.center,
+     map: map.instance
+   });
+ });
 });
 
 Template.detail.helpers({
@@ -7,5 +14,20 @@ Template.detail.helpers({
   },
   isFirst: function(){
     return Properties.findOne({}).images[0] == this;
+  },
+  isRental: function(){
+    return this.type === 'rental';
+  },
+  locationPropertyOptions: function() {
+    if (GoogleMaps.loaded()) {
+      return {
+        center: new google.maps.LatLng(this.location.lat, this.location.lng),
+        zoom: 14
+      };
+    }
   }
 });
+
+Meteor.startup(function() {
+    GoogleMaps.load({ v: '3', key: 'AIzaSyBL7BvwkVTas7pM6uIKuzYAudCVTmVGcqY', libraries: 'geometry,places' });
+  });
