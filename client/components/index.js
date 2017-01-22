2685294227dddcbea6f766cad7ded83847f91f7e
Laura <laura@Lauras-MacBook-Pro.local>
2017-01-22 20:23:57 +0100

    Added location search with mock lat and lng

--- client/components/index.js
+++ client/components/index.js
@@ -31,9 +31,27 @@ Template.index.helpers({
         FlowRouter.go('/properties?type=' + type + '&matchSearch=' + matchSearch);
       }
     },
+
+    'click #searchByLocation' : function () {
+      var type = $("input[name='propertyTypeLocation']:checked").val();
+      var correctSearch = true;
+      if (!type){
+          $("#searchDivLocation").find(".search-radio-container label").addClass("search-radio-required");
+          correctSearch = false;
+      } else {
+          $("#searchDivLocation").find(".search-radio-container label").removeClass("search-radio-required");
+      }
+
+      if (correctSearch){
+        FlowRouter.go('/properties?type=' + type + '&lat=' + 41.3869693 + '&lng=' + 2.1679476);
+      }
+
+    },
+
     'click input[type="radio"]' : function () {
       $(".search-radio-container label").removeClass("search-radio-required");
     },
+
     'keyup #matchSearch': function () {
       if ($("#matchSearch").val()){
         $("#matchSearch").removeClass("search-input-required");
